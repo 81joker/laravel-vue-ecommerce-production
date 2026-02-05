4a5ff0a39c8d83362f1cdb3e569062e73607e3a5
Nehad <35563834+81joker@users.noreply.github.com>
2026-02-05 14:27:33 +0100

    Refactor code structure for improved readability and maintainability

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,8 +13,7 @@ class User extends Authenticatable
     use HasApiTokens;
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory;
-    use Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
