1dc2b1cc58a197b518302c04dacc3021d13aacc1
Nehad <35563834+81joker@users.noreply.github.com>
2026-02-05 18:25:44 +0100

    LVEP-2 Delete the code not necessary from Route web.php

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 // FIXME:   @nrhad.al.timimi@gmail.com @Nehad you should control this router and delete it if you don't need it, because it's a security risk to have an open route like this
-// TODO:   @nrhad.al.timimi@gmail.com @Nehad you should control this router and delete it if you don't need it, because it's a security risk to have an open route like this
+// TODO:  XXX
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
@@ -45,4 +45,4 @@ Route::middleware(['auth', 'verified'])->group(function () {
 //     Route::delete('/profiles', [ProfileController::class, 'destroy'])->name('profile.destroy');
 // });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
